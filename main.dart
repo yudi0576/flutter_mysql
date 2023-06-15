046272967566ed6eb8e541f0a882a43591ad7288
yudi0576 <129965125+yudi0576@users.noreply.github.com>
2023-06-15 23:07:39 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter MySQL',
+      debugShowCheckedModeBanner: false,
       home: Home(),
     );
   }
